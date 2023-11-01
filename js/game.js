5167e0e0920e2d89f425605a98a089ab3e5fad0e
Viacheslav <vyac.v.kuznetsov@rt.ru>
2023-11-01 18:25:18 +0300

    feat-going-circle + requestanimationframe

--- js/game.js
+++ js/game.js
@@ -1,5 +1,14 @@
-function loop(){
-	window.game.update();
+let previousTimeStamp;
+
+function loop(timeStamp = 0){
+	if (previousTimeStamp === undefined) {
+		previousTimeStamp = timeStamp;
+	  }
+
+	const count = timeStamp - previousTimeStamp;
+	previousTimeStamp = timeStamp;
+
+	window.game.update(count);
 	requestAnimationFrame(loop);
 }
 
@@ -24,6 +33,11 @@ class Game{
 		
 		this.sprite1Pos = new Point();
 		this.sprite2Pos = new Point();
+
+		this.center = new Point(150, 50);
+
+		this.radius = 50;
+		this.angle = 30;
 		
 		this.sprite1Frame = new Point();
 		this.sprite2Frame = new Point();
@@ -37,7 +51,7 @@ class Game{
 		this.worldSpaceMatrix = new M3x3().transition(-1, 1).scale(2/wRatio,-2/240);
 	}
 	
-	update(){
+	update(count){
 		this.gl.viewport(0,0, this.canvasElm.width, this.canvasElm.height);
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 		
@@ -50,7 +64,9 @@ class Game{
 		this.sprite2Frame.x = (new Date() * 0.006) % 3;
 		this.sprite2Frame.y = (new Date() * 0.002) % 2;
 		
-		this.sprite2Pos.x = (this.sprite2Pos.x + 0.1) % 256;
+		this.sprite2Pos.x = this.center.x + this.radius * Math.cos(this.angle);
+		this.sprite2Pos.y = this.center.y + this.radius * Math.sin(this.angle);
+		this.angle = this.angle + 0.0006 * count;
 		
 		this.sprite1.render(this.sprite1Pos, this.sprite1Frame);
 		this.sprite2.render(this.sprite2Pos, this.sprite2Frame);
