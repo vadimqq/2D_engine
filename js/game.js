94dd0bc036248941406f924288359334d5bcf5f2
Vadim Silantev <vadim.silantev@rt.ru>
2023-11-03 11:10:17 +0300

    решение по вращаению объектов

--- js/game.js
+++ js/game.js
@@ -1,14 +1,22 @@
-function loop(){
-	window.game.update();
+function loop(timeStamp = 0){
+	window.game.update(timeStamp);
 	requestAnimationFrame(loop);
 }
+
+let D_for_sprite = 0
+const radius_for_sprite = 30
+const speed_for_sprite = 0.0005
+
+let center_point = null
+let date = 0
 
 class Game{
 	constructor(){
 		this.canvasElm = document.createElement("canvas");
 		this.canvasElm.width = 800;
 		this.canvasElm.height = 600;
-		
+		this.lastTime = 0;
+		this.requiredElapsed = 1000 / 10;
 		this.worldSpaceMatrix = new M3x3();
 		
 		this.gl = this.canvasElm.getContext("webgl2");
@@ -22,11 +30,14 @@ class Game{
 		this.sprite1 = new Sprite(this.gl, "img/manonfire.png", vs, fs, {width:48, height:48});
 		this.sprite2 = new Sprite(this.gl, "img/walker.png", vs, fs, {width:64, height:64});
 		
+		center_point = new Point(0, 0)
+
 		this.sprite1Pos = new Point();
 		this.sprite2Pos = new Point();
 		
 		this.sprite1Frame = new Point();
 		this.sprite2Frame = new Point();
+
 	}
 	
 	resize(x,y){
@@ -36,24 +47,36 @@ class Game{
 		let wRatio = x / (y/240);
 		this.worldSpaceMatrix = new M3x3().transition(-1, 1).scale(2/wRatio,-2/240);
 	}
-	
-	update(){
+	getDelta(timeStamp) {
+		const elapsed = timeStamp - this.lastTime;
+		if (elapsed > this.requiredElapsed) {
+			this.lastTime = timeStamp;
+		}
+		return elapsed
+	}
+	update(timeStamp){
+		const delta = this.getDelta(timeStamp)
+		D_for_sprite -= delta
+		date += delta * 0.001
 		this.gl.viewport(0,0, this.canvasElm.width, this.canvasElm.height);
 		this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 		
 		this.gl.enable(this.gl.BLEND);
 		this.gl.blendFunc(this.gl.SRC_ALPHA, this.gl.ONE_MINUS_SRC_ALPHA);
 		
-		this.sprite1Frame.x = (new Date() * 0.006) % 3;
-		this.sprite1Frame.y = (new Date() * 0.002) % 2;
+		this.sprite1Frame.x = (date) % 3;
+		this.sprite1Frame.y = (date) % 2;
 		
-		this.sprite2Frame.x = (new Date() * 0.006) % 3;
-		this.sprite2Frame.y = (new Date() * 0.002) % 2;
+		this.sprite2Frame.x = (date) % 3;
+		this.sprite2Frame.y = (date) % 2;
+
+		this.sprite2Pos.x = Math.sin(D_for_sprite * speed_for_sprite) * radius_for_sprite + center_point.x
+		this.sprite2Pos.y = Math.cos(D_for_sprite * speed_for_sprite) * radius_for_sprite + center_point.y
+
 		
-		this.sprite2Pos.x = (this.sprite2Pos.x + 0.1) % 256;
-		
-		this.sprite1.render(this.sprite1Pos, this.sprite1Frame);
 		this.sprite2.render(this.sprite2Pos, this.sprite2Frame);
+		this.sprite1.render(this.sprite1Pos, this.sprite1Frame);
+
 		
 		this.gl.flush();
 	}
