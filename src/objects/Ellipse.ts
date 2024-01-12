708760f9152c5dca058397698c63512c9e30916c
Viacheslav <vyac.v.kuznetsov@rt.ru>
2024-01-12 17:11:40 +0300

    task_5 ellipse class with inner radius

--- src/objects/Ellipse.ts
+++ src/objects/Ellipse.ts
@@ -4,24 +4,35 @@ import { Color } from "../core/Color";
 import { Object2D } from "../core/Object2D";
         
 const radius = 50
-const segment = 100
+const innerRadius = 20;
+const segment = 100;
+const length = Math.PI * 2;
+const coefficient = length === Math.PI * 2 ? 2 : 0
 
 export class Ellipse extends Object2D {
     constructor() {
-        const points = [0, 0]
-        const indices = []
-        const step = Math.PI * 2 / segment
+        const points = [Math.sin(0) * innerRadius, Math.cos(0) * innerRadius];
+        const indices = [];
+        const step = length / segment;
 
-        for (let i = 0; i <= segment; i++) {
-            const cos = Math.cos(step * i)
-            const sin = Math.sin(step * i)
-
-            const pointX = sin  * radius;
-            const pointY = cos  * radius;
-
-           points.push(pointX, pointY)
-
-           indices.push(0, i + 1, i + 2)
+        for (let i = 0; i <= segment + coefficient; i++) {
+            if (i % 2 === 0) {
+                const cos = Math.cos(step * i)
+                const sin = Math.sin(step * i)
+    
+                const pointX = sin  * radius;
+                const pointY = cos  * radius;
+                points.push(pointX, pointY);
+            } else {
+                const cos = Math.cos(step * i)
+                const sin = Math.sin(step * i)
+    
+                const pointX = sin  * innerRadius;
+                const pointY = cos  * innerRadius;
+    
+                points.push(pointX, pointY);
+            }
+           indices.push(i, i + 1, i + 2)
         }
 
         console.log({points, indices})
